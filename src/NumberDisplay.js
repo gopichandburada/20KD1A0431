c845b7357502b994eadc5103d24a63fb34d99c6b
gopi <gopiburada@gmail.com>
2023-08-16 19:55:24 +0530

    first

---
+++ src/NumberDisplay.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+function NumberDisplay() {
+  const [numbers, setNumbers] = useState([]);
+
+  const fetchData = async () => {
+    const urls = [
+      'http://20.244.56.144/numbers/primes',
+      'http://20.244.56.144/numbers/fibo',
+      'http://20.244.56.144/numbers/odd',
+      'http://20.244.56.144/numbers/rand'
+    ];
+
+    try {
+      const responses = await Promise.all(
+        urls.map(url => fetch(url).then(response => response.json()).catch(error => ({ numbers: [] })))
+      );
+
+      const validResponses = responses.filter(response => response.numbers && Array.isArray(response.numbers));
+
+      const mergedNumbers = validResponses.flatMap(item => item.numbers);
+      const uniqueNumbers = Array.from(new Set(mergedNumbers));
+      const sortedNumbers = uniqueNumbers.sort((a, b) => a - b);
+
+      setNumbers(sortedNumbers);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
+  return (
+    <div>
+      <div>
+        <h2>Numbers:</h2>
+        <pre>{JSON.stringify({ numbers }, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
+
+export default NumberDisplay;
